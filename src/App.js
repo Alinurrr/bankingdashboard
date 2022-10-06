69ad6ddc5dd22e6d9132354b8bff2c788297f90f
Alinurrr <alinoer.rachman@gmail.com>
2022-10-06 17:04:30 +0700

    insert logo assets and change favicon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    document.title = "Dashboard GND Bank";
+  }, []);
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
