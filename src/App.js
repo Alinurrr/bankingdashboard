0c9f60db82a358e6988a5bb93ebd4a3ad95e71c4
Alinurrr <alinoer.rachman@gmail.com>
2022-10-09 23:12:53 +0700

    insert 404 page for display lower than 1536px for now

--- src/App.js
+++ src/App.js
@@ -8,11 +8,26 @@ function App() {
     document.title = "Dashboard GND Bank";
   }, []);
   return (
-    <div className="flex w-full bg-white">
-      <Sidebar />
-      <div className="flex flex-col w-full overflow-y-auto">
-        <Navbar />
-        <IndexContent />
+    <div>
+      <div className="2xl:flex w-full bg-white hidden">
+        <Sidebar />
+        <div className="flex flex-col w-full overflow-y-auto">
+          <Navbar />
+          <IndexContent />
+        </div>
+      </div>
+
+      <div className="2xl:hidden h-screen place-content-center bg-white grid">
+        <div className="text-center">
+          <strong className="text-9xl font-black text-gray-200">404</strong>
+          <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Ah, realy sorry :'(
+          </h1>
+          <p className="mt-4 text-gray-500">This dashboard can only be used on desktops with sizes more than 1536px for now.</p>
+          <button onClick={() => { window.location.assign('https://alinur.my.id'); }} className="mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring">
+            See Another Project
+          </button>
+        </div>
       </div>
 
     </div>
